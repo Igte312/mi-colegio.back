adb1bc7482fdb9c09b43b8e0596def2fb3c5b4a4
José Ignacio López <jlopez@acl.cl>
2025-10-04 22:02:39 -0300

    changes in pom and structure

--- src/main/java/com/micolegio/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.micolegio.repositories;
-//
-//import com.micolegio.domain.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User, Integer> {
-//
-//    User findByEmail(String email);
-//}
